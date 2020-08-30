52b8ba03a27759dd873242f872ccf8c3925ee5da
InsertWittyCommentHere <69561476+InsertWittyCommentHere@users.noreply.github.com>
2020-08-30 16:18:36 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,31 @@ const Engine = Matter.Engine
 const World = Matter.World
 const Bodies = Matter.Bodies
 const Body = Matter.Body
+class Box{
+    constructor(x, y, width, height){
+    var options = {
+        'restitution':0,
+        'friction':1.0,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill(255, 0, 0);
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
 
 function preload()
 {
